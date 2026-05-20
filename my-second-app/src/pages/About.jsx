ac083556aaeb22cf7cf98cbdadee634f479232d2
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-05-20 19:50:45 +0530

    Basic UI

---
+++ my-second-app/src/pages/About.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <>
+    
+    <div style={{ padding: "20px" }}>
+      <h1>About Us</h1>
+
+      <p>
+        Welcome to our website! This is a simple React application created
+        to learn and practice frontend development.
+      </p>
+
+      <p>
+        We use React, Bootstrap, and React Router to build responsive
+        and user-friendly web pages.
+      </p>
+
+      <p>
+        This page gives basic information about the website.
+      </p>
+    </div>
+
+    </>
+  );
+};
+
+export default Footer;
